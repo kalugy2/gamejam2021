a02b085a74c9d0a55298821fd6af3eb1b4ed4e81
HelperHelper <arangojulian93@gmail.com>
2021-01-31 18:40:55 -0500

    Se sube la ultima versión de presentación

--- src/index.js
+++ src/index.js
@@ -28,20 +28,30 @@ var mons1;
 var walk1;
 var mons2;
 var walk2;
+var key;
+var textWin;
+var audmonster;
+var audwingame;
+var audcatch;
 
 var game = new Phaser.Game(config);
 
 function preload ()
 {
     this.load.image('tiles', [ 'assets/brick04.png', 'assets/drawtiles1_n.png' ]);
+    this.load.image('key', 'assets/key.png');
     this.load.tilemapCSV('map', 'assets/grid.csv');
     this.load.audio('atmosfera', 'assets/audio/gamejamformat.mp3');
-    this.load.spritesheet('lui', 'assets/luisPr.png', 
-    { frameWidth: 58, frameHeight: 58 });
+    this.load.audio('monster', 'assets/audio/vozenemigogamejam.mp3');
+    this.load.audio('win', 'assets/audio/wingamejam.mp3');
+    this.load.audio('catch', 'assets/audio/atrapar.mp3')
+    this.load.spritesheet('lui', 'assets/luisSp.png', 
+    { frameWidth: 45, frameHeight: 32 });
     this.load.spritesheet('monst1', 'assets/enemie1.png', 
     { frameWidth: 58, frameHeight: 58 });
     this.load.spritesheet('monst2', 'assets/enemigo2.png', 
-    { frameWidth: 91, frameHeight: 91 });
+    { frameWidth: 79, frameHeight: 91 });
+    
    
 }
 
@@ -49,26 +59,33 @@ function create ()
 {
     var map = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
 
-    
-
     var tileset = map.addTilesetImage('tiles', null, 32, 32, 1, 2);
 
     layer = map.createLayer(0, tileset, 0, 0).setPipeline('Light2D');
 
-     player = this.physics.add.sprite(58, 58, 'lui');
-    // player = this.add.image(32+16, 32+16, 'car');
+    key = this.add.image(748, 334, 'key');
+    key.setDataEnabled();
+    key.data.set('key', 0);
+
+    key.visible = false;
+
+    player = this.physics.add.sprite(47, 47, 'lui');
+    
     mons1 = this.add.sprite(300, 58, 'monst2');
 
-    // mons1.animations.add('walk');
-
-    // mons1.animations.play('walk', 30 , true);
+    mons1.visible=false;
+  
     cursors = this.input.keyboard.createCursorKeys();
 
     let audio = this.sound.add('atmosfera', {loop: true});
+    audmonster = this.sound.add('monster');
+    audwingame = this.sound.add('win');
+    audcatch = this.sound.add('catch');
 
     audio.play();
-
-    light = this.lights.addLight(0, 0, 100);
+ 
+     light = this.lights.addLight(0, 0, 100);
+     this.lights.enable().setAmbientColor(0x000111);
 
     this.cameras.main.setBounds(0, 0, layer.displayWidth, layer.displayHeight);
     this.cameras.main.startFollow(player);
@@ -77,11 +94,13 @@ function create ()
 
     llave = this.add.text(155, 10, 'key: 0/1', { fontSize: '19px', fill: '#9B8E78' });
 
-    this.lights.enable().setAmbientColor(0x000111);
+    textWin = this.add.text(400,2000,'you win!', { fontSize: '20px', fill: '#82A07D' });
+
+    textWin.visible = false;
 
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('lui', { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers('lui', { start: 0, end: 2 }),
         frameRate: 10,
         repeat: -1
     });
@@ -103,13 +122,39 @@ function create ()
         frameRate: 10,
         repeat: -1
     });
+    this.anims.create({
+        key: 'walk',
+        frames: this.anims.generateFrameNumbers('monst2', {start: 0, end: 1 }),
+        frameRate: 3,
+        repeat: -1
+    });
 
 
     offsets = [ 0.1, 0.3, 0.5, 0.7 ];
 }
 
+var isCharacterWithKey = 0;
+
 function update ()
 {
+
+    if(player.x >= 700 && player.y >= 330){
+        key.visible = true;
+    }else{
+        key.visible = false;
+    }
+
+    if(player.x >= 255 && player.y >= 58){
+        audmonster.play();
+        mons1.visible = true;
+        mons1.anims.play('walk', true);
+   }else 
+   {
+      mons1.visible = false;
+      mons1.anims.stop();
+   }
+
+
     if (this.input.keyboard.checkDown(cursors.left, 100))
     {
         var tile = layer.getTileAtWorldXY(player.x - 32, player.y, true);
@@ -120,6 +165,7 @@ function update ()
         }
         else
         {
+           
             player.x -= 32;
             player.angle = 180;
         }
@@ -127,10 +173,21 @@ function update ()
     else if (this.input.keyboard.checkDown(cursors.right, 100))
     {
         var tile = layer.getTileAtWorldXY(player.x + 32, player.y, true);
-
+        
         if (tile.index === 2)
         {
             //  Blocked, we can't move
+        }
+        else if (tile.index === 3 && isCharacterWithKey == 0) {
+       
+            llave.destroy();
+            llave = this.add.text(155, 10, 'key: 1/1', { fontSize: '19px', fill: '#9B8E78' });
+            key.destroy();
+            isCharacterWithKey = 1;
+            player.x += 32;
+            player.angle = 0;
+            audcatch.play();
+
         }
         else
         {
@@ -159,6 +216,25 @@ function update ()
         if (tile.index === 2)
         {
             //  Blocked, we can't move
+        }
+        else if (tile.index === 4) {
+            if (isCharacterWithKey == 1 ) {
+                textWin.visible = true;
+                audwingame.play();
+                player.y += 32;
+                player.angle = 90;
+                let time = 15;
+                for(time; time<=0; time--){
+                    console.log(time);
+                 
+                }
+                {
+                  this.scene.restart();
+                }
+            }
+            else {
+                console.log("NOT KEY")
+            }
         }
         else
         {
